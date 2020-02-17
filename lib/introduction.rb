2255f625fc9f1eecad8af569bbaa7a6080873ee3
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-02-17 21:24:58 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,3 +4,8 @@ end
 
 # introduction("Holly")
 # introduction("Alex")
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I'm learning to program in #{language}"
+end
+
